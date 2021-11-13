415075fe9f399e2fcc8bec778fbeea82e6b3f0b3
RodrigoAN97 <rodrigo_neves97@outlook.com>
2021-11-13 12:50:02 -0300

    fix: :bug:

--- src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: false,
-  mapbox: {
-    accessToken:
-      'pk.eyJ1Ijoicm9kcmlnb2FuOTciLCJhIjoiY2t2eHVyaXViMHdqZzJubzF4bjgzdDVjaCJ9.kjdodOQ3TaZ53NjfkrwEFQ',
-  },
-  mapTiler: { key: 'SoL71Zyf7SmLrVYWC7fQ' },
-};
