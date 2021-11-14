dced8d57468d00a1e06e341e5fa97afc06ce8874
RodrigoAN97 <rodrigo_neves97@outlook.com>
2021-11-14 20:45:08 -0300

    style: :lipstick:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,10 @@ import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [AppComponent, MapComponent],
@@ -14,6 +18,10 @@ import { MatSidenavModule } from '@angular/material/sidenav';
     AppRoutingModule,
     NoopAnimationsModule,
     MatSidenavModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatListModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
